80a69928566b3b670968452a1338a50f1ee6d114
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-04-15 23:31:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
 
     post '/' do 
       
-     @puppy =  Puppy.new(params[:name])
+     @puppy =  Puppy.new(params[:name, :breed, :age])
       erb:display_puppy.erb
     end
 end
